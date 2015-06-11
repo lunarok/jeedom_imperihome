53a0a65670ec8b448b2015480faa9eb3e6a08d79
loic <lgevrey@oxya.com>
2015-06-11 11:50:07 +0200

    UI improvement

--- desktop/js/imperihome.js
+++ desktop/js/imperihome.js
@@ -14,6 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with Jeedom. If not, see <http://www.gnu.org/licenses/>.
  */
+
  $('.bt_saveISSConfig').on('click',function(){
     var imperihome = {};
     $('tr.imperihome').each(function(){
