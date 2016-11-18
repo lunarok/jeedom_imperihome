cf8b528764deb9077047ca8130bc5768fba251d0
Cédric Dupont <lunarok@gmail.com>
2016-11-18 15:04:47 +0100

    array

--- core/ajax/imperihome.ajax.php
+++ core/ajax/imperihome.ajax.php
@@ -44,7 +44,7 @@ try {
     if (init('action') == 'loadAdvancedDeviceISSConfig') {
         $deviceId = init('deviceId');
         $issAdvancedConfig = imperihome::getIssAdvancedConfig();
-        if(array_key_exists($deviceId, $issAdvancedConfig) && !empty($issAdvancedConfig)){
+        if(array_key_exists($deviceId, $issAdvancedConfig)){
             $cmd = cmd::byId($deviceId);
             if(is_object($cmd)){
                 $issAdvancedConfig[$deviceId]['humanName'] = $cmd->getHumanName();
